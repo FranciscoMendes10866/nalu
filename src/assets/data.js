dc18cc93984b59923a4aaa95bf676c6d80a1fc00
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-07-22 22:16:40 +0100

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,45 @@
+export const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "age",
+    label: "AGE",
+  },
+];
+
+export const rows = [
+  {
+    key: "1",
+    firstName: "Tony",
+    lastName: "Reichert",
+    role: "Developer",
+    age: "35",
+  },
+  {
+    key: "2",
+    firstName: "Zoey",
+    lastName: "Lang",
+    role: "Designer",
+    age: "22",
+  },
+  {
+    key: "3",
+    firstName: "Jane",
+    lastName: "Fisher",
+    role: "CEO",
+    age: "29",
+  },
+  {
+    key: "4",
+    firstName: "William",
+    lastName: "Howard",
+    role: "Designer",
+    age: "27",
+  },
+];
